b617197218a8522dbe34d841f6d2d5cf49b2d0c5
Michał Czapliński <czapli@o2.pl>
2022-08-07 10:55:25 +0200

    32.1 Swagger, OpenAPI

--- src/main/java/com/crud/tasks/config/CoreConfiguration.java
+++ src/main/java/com/crud/tasks/config/CoreConfiguration.java
@@ -3,7 +3,9 @@ package com.crud.tasks.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
+@EnableWebMvc
 @Configuration
 public class CoreConfiguration {
 
