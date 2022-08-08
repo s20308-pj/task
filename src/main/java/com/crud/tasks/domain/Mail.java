c960a304db594c68ff189ba1f57660b7017b7b15
Michał Czapliński <czapli@o2.pl>
2022-08-08 13:23:53 +0200

    23.3 add CC to mail, add @Buliderin mail

---
+++ src/main/java/com/crud/tasks/domain/Mail.java
@@ -0,0 +1,18 @@
+package com.crud.tasks.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import org.springframework.mail.SimpleMailMessage;
+
+import java.util.Optional;
+
+@Getter
+@AllArgsConstructor
+@Builder
+public class Mail{
+    private final String mailTo;
+    private final String subject;
+    private final String message;
+    private final String toCc;
+}
